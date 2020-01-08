e361c1c3d67b4d8dc06c7a8808cbb36a9973e696
Franz Bascope <franzbascope123@gmail.com>
2020-01-08 16:53:58 -0400

    Updates

---
+++ precache-manifest.e26dd6a6cd39db3be8e37d2fffc5a211.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e01d571e28ed843f793cf39b63a5cae",
+    "url": "/Smart-Access-UI/index.html"
+  },
+  {
+    "revision": "4b22818f1db09161e13b",
+    "url": "/Smart-Access-UI/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "e1afc5c2996280cc9564",
+    "url": "/Smart-Access-UI/static/css/main.f02fc37c.chunk.css"
+  },
+  {
+    "revision": "4b22818f1db09161e13b",
+    "url": "/Smart-Access-UI/static/js/2.ea772890.chunk.js"
+  },
+  {
+    "revision": "6962366128cd0fe7434b0477d3387197",
+    "url": "/Smart-Access-UI/static/js/2.ea772890.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1afc5c2996280cc9564",
+    "url": "/Smart-Access-UI/static/js/main.5eda8957.chunk.js"
+  },
+  {
+    "revision": "e65d685fdcb1689b5bac",
+    "url": "/Smart-Access-UI/static/js/runtime-main.0a6dbfd4.js"
+  },
+  {
+    "revision": "507d63c834a50568c2cb61fbc3fcf1b1",
+    "url": "/Smart-Access-UI/static/media/logo.507d63c8.png"
+  }
+]);
